376da218219507b96290b4ada9a25eed3e64cb50
tjheckler <timothyheckler@gmail.com>
2018-05-11 10:28:09 -0500

    Silver done, gold started some code is now broken, may or may not finish gold attempt

--- src/com/company/FundAllocationTool.java
+++ src/com/company/FundAllocationTool.java
@@ -12,7 +12,7 @@ public class FundAllocationTool
     List<String> stringBuffer = new LinkedList<>();
     final String[] investmentOptions = {"End Of World 2012", "Super Risky Optimists", "Y2K Survivors", "End Of Time 2038"};
     Employee employee;
-
+    double percentage;
     String name = "";
 
     public static void main(String[] args)
@@ -23,6 +23,7 @@ public class FundAllocationTool
 
     void run()
     {
+
         System.out.println("Welcome to the 401(k) fund allocation tool. Enter your first and last name or exit to quit.");
 
         //Set<String> investmentOptions = new HashSet<>();
@@ -33,118 +34,122 @@ public class FundAllocationTool
 
         do
         {
-
             name = scanner.nextLine();
-
-            if (!name.equalsIgnoreCase("exit"))
-            {
-                System.out.println("Welcome, " + name + "! What is your employee ID?");
+            System.out.println("Welcome, " + name + "! What is your employee ID?");
 
 //Copied
-                String employeeId = scanner.nextLine();
+            String employeeId = scanner.nextLine();
 
-                System.out.println("How much per month would you like to add to your 401(k)? (amount must be between $10 and $200)");
+            System.out.println("How much per month would you like to add to your 401(k)? (amount must be between $10 and $200)");
 //Copied start
-                BigDecimal min = new BigDecimal("9");
-                BigDecimal max = new BigDecimal("201");
-                BigDecimal amountAdded;
-                BigDecimal employerContribution;
+            BigDecimal min = new BigDecimal("9");
+            BigDecimal max = new BigDecimal("201");
+            BigDecimal amountAdded = new BigDecimal("0");
+            BigDecimal employerContribution;
+            Employee employee = new Employee(name, amountAdded, employeeId, buffer);
 //Copied end
+
+            do
+            {
+                //copied   amountadded variable
+                amountAdded = scanner.nextBigDecimal();
+                scanner.nextLine();
+
+                if ((amountAdded.compareTo(min) <= 0) || (amountAdded.compareTo(max) >= 0))
+                {
+                    System.out.println("Please enter a value between 10 and 200");
+                }
+
+                if (((amountAdded.compareTo(new BigDecimal("50")) <= 0)))
+                {
+                    employerContribution = amountAdded.multiply(new BigDecimal(".5").setScale(2, RoundingMode.HALF_UP));
+                    amountAdded = amountAdded.add(employerContribution);
+                    System.out.println("Your employee contributed $" + employerContribution);
+                    System.out.println("Your total amount to invest is $" + amountAdded);
+                } else
+                {
+                    employerContribution = new BigDecimal("50").multiply(new BigDecimal(".5").setScale(2, RoundingMode.HALF_UP));
+                    amountAdded = amountAdded.add(employerContribution);
+                    System.out.println("Your employer contributed $" + employerContribution);
+                    System.out.println("Your total amount to invest is $" + amountAdded);
+                }
+
+                System.out.println("Which programs would you like to invest in?");
+                printInvestmentOptions();
+//Copied Start
+                int investmentChoice = 1;
+                String investmentName = "";
+                double percentTotal = 0;
+                BigDecimal investmentTotal = new BigDecimal("0");
+//Copied End
                 do
                 {
-                    amountAdded = scanner.nextBigDecimal();
+                    investmentChoice = scanner.nextInt();
                     scanner.nextLine();
+//Picking Funds Selection------------------------------------------------------------------------------------------------------------------------
+                    if (investmentChoice > 0 && investmentChoice <= investmentOptions.length)
+                    {
+                        investmentName = investmentOptions[investmentChoice - 1];
 
-                    if ((amountAdded.compareTo(min) <= 0) || (amountAdded.compareTo(max) >= 0))
-                    {
-                        System.out.println("Please enter a value between 10 and 200");
-                    } else
-                    {
-                        if (((amountAdded.compareTo(new BigDecimal("50")) <= 0)))
+                        if (stringBuffer.contains(investmentName))
                         {
-                            employerContribution = amountAdded.multiply(new BigDecimal(".5").setScale(2, RoundingMode.HALF_UP));
-                            amountAdded = amountAdded.add(employerContribution);
-                            System.out.println("Your employee contributed $" + employerContribution);
-                            System.out.println("Your total amount to invest is $" + amountAdded);
+                            System.out.println("You are already investing in that fund. Pick another one.");
                         } else
                         {
-                            employerContribution = new BigDecimal("50").multiply(new BigDecimal(".5").setScale(2, RoundingMode.HALF_UP));
-                            amountAdded = amountAdded.add(employerContribution);
-                            System.out.println("Your employer contributed $" + employerContribution);
-                            System.out.println("Your total amount to invest is $" + amountAdded);
-                        }
-
-                        System.out.println("Which programs would you like to invest in?");
-                        printInvestmentOptions();
-//Coppied Start
-                        int investmentChoice = 1;
-                        String investmentName = "";
-                        double percentTotal = 0;
-                        BigDecimal investmentTotal = new BigDecimal("0");
-//Copied End
-                        do
-                        {
-                            investmentChoice = scanner.nextInt();
+                            stringBuffer.add(investmentName);
+                            System.out.println("What percentage would you like to add to " + investmentName + "?");
+                            percentage = scanner.nextDouble();
                             scanner.nextLine();
-
-                            if (investmentChoice > 0 && investmentChoice <= investmentOptions.length)
-                            {
-                                investmentName = investmentOptions[investmentChoice - 1];
-
-                                if (stringBuffer.contains(investmentName))
-                                {
-                                    System.out.println("You are already investing in that fund. Pick another one.");
-                                } else
-                                {
-                                    stringBuffer.add(investmentName);
-                                    System.out.println("What percentage would you like to add to " + investmentName + "?");
-                                    double percentage = scanner.nextDouble();
-                                    scanner.nextLine();
-                                    percentTotal += percentage;
-
-                                    if (percentTotal > 100)
-                                    {
-                                        percentTotal -= percentage;
-                                        System.out.println("Percentage has exceed 100%. You have " + (100 - percentTotal) + "% left to invest.");
-                                        System.out.println("Enter another investment.");
-                                        printInvestmentOptions();
-                                    } else
-                                    {
-                                        BigDecimal investmentAmount = amountAdded.multiply(new BigDecimal(percentage * .01));
-
-                                        InvestmentOptions investmentOption = new InvestmentOptions(investmentOptions[investmentChoice - 1], percentage, investmentAmount);
-
-                                        buffer.add(investmentOption);
-
-                                        System.out.println(investmentOptions[investmentChoice - 1] + " added to your list of investments.");
-                                        System.out.println("You have allocated " + percentTotal + "% of your funds. " + (100 - percentTotal) + "% remains.");
-                                        System.out.println("Enter another investment, or enter -1 to finish.");
-                                        printInvestmentOptions();
-                                    }
-                                }
-                            } else if (investmentChoice > investmentOptions.length)
-                            {
-                                System.out.println("No such investment. Try again.");
-                            } else if (investmentChoice != -1)
-                            {
-                                System.out.println("Invalid input. Try again.");
-                            }
-
-                        } while (investmentChoice != -1 || percentTotal != 100);
-                        {
-
-                            System.out.println("Printing all investment information: ");
-
-                            Employee employee = new Employee(name, amountAdded, employeeId, buffer);
-
-                            listOfEmployees.add(employee);
-                            printEmployee(employee);
+                            percentTotal += percentage;
                         }
+                        if (investmentChoice > investmentOptions.length)
+                        {
+                            System.out.println("No such investment. Try again.");
+                        }
+                        if (investmentChoice != -1)
+                        {
+                            System.out.println("Invalid input. Try again.");
+                        }
+//Percent over 100-------------------------------------------------------------------------------------------------------------------------------
+                        if (percentTotal > 100)
+                        {
+                            percentTotal -= percentage;
+                            System.out.println("Percentage has exceed 100%. You have " + (100 - percentTotal) + "% left to invest.");
+                            System.out.println("Enter another investment.");
+                            printInvestmentOptions();
+                        } else
+                        {
+                            BigDecimal investmentAmount = amountAdded.multiply(new BigDecimal(percentage * .01));
 
+                            InvestmentOptions investmentOption = new InvestmentOptions(investmentOptions[investmentChoice - 1], percentage, investmentAmount);
 
+                            buffer.add(investmentOption);
+
+                            System.out.println(investmentOptions[investmentChoice - 1] + " added to your list of investments.");
+                            System.out.println("You have allocated " + percentTotal + "% of your funds. " + (100 - percentTotal) + "% remains.");
+                            System.out.println("Enter another investment, or enter -1 to finish.");
+                            printInvestmentOptions();
+                        }
                     }
-                } while ((amountAdded.compareTo(min) <= 0) || (amountAdded.compareTo(max) >= 0));
+                }
+                //Copied Start
+                while (investmentChoice != -1 || percentTotal != 100);
+                {
+                    System.out.println("Printing all investment information: ");
+//copied create employee
+
+
+                    listOfEmployees.add(employee);
+                    printEmployee(employee);
+                }
+//Copied End
+
+
             }
+            while ((amountAdded.compareTo(min) <= 0) || (amountAdded.compareTo(max) >= 0));
+
+
+//WTF Start
             if (!name.equalsIgnoreCase("EXIT"))
                 System.out.println("Enter another name to start another account.");
             else
@@ -152,18 +157,20 @@ public class FundAllocationTool
         }
 
         while (!name.equalsIgnoreCase("EXIT"));
-        {}
+        {
+        }
 
         if (name.equalsIgnoreCase("PRINT"))
         {
             System.out.println(listOfEmployees);
         }
+//WTF END
         buffer.clear();
         stringBuffer.clear();
 
     }
 
-//Copied Start
+    //Copied Start
     void printInvestmentOptions()
     {
         System.out.println("Here are your investment options: ");
